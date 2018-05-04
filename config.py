7b7742793f9ea9351298dd92f360232c58fcc186
Rain <364687927@qq.com>
2018-05-04 20:54:29 +0800

    fix  bug

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+# 开发环境
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:100200@localhost:3306/machinelearning"
+    FILE_BASE_PATH = 'D:/Mine/Code/Jetbrains/pycharm/file_resources'
+
+
+# 测试环境
+class TestingConfig(Config):
+    TESTING = True
+
+
+# 生产环境
+class ProductionConfig(Config):
+    PRODUCTION = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:ZZy-1998@127.0.0.1:3306/deep_id"
+    FILE_BASE_PATH = '/root/file_resource'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
